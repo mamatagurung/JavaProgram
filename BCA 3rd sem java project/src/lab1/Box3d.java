08246b9c98b226ca03d494e95b6fd0be6546e573
mamatagurung <mamatagurung190@gmail.com>
2022-02-02 22:51:40 +0545

    file

--- BCA 3rd sem java project/src/lab1/Box3d.java
+++ BCA 3rd sem java project/src/lab1/Box3d.java
@@ -1,21 +1,33 @@
-package lab1;
+package one;
+
 class Box{
-	int lenght=5 ;
-	int breadth=7 ;;
+	int length;
+	int breadth ;
+	int height;
 	
+	Box(){
+		System.out.println("Constructor called");
+	}
 	
 }
 public class Box3d extends Box{
-	int height =9;
+	Box3d(int length, int breadth, int height){
+		this.length = length;
+		this.breadth = breadth;
+		this.height = height;
+		}
+	public Box3d(){
+	System.out.println("Consturctor called");
+	}
 	int volume(){
-		return lenght*breadth*height;
+		return length*breadth*height;
 	}
 	int area() {
-		return 2*(lenght*breadth+lenght*height+breadth*height);
+		return 2*(length*breadth+length*height+breadth*height);
 	}
 	
 public static void main(String args[]) {
-	Box3d obj = new Box3d();
+	Box3d obj = new Box3d(4,6,8);
 	System.out.println("the volume of Box3d is "+obj.volume());
 	System.out.println("The area of Box3d is "+obj.area());
 }
